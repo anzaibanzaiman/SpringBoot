8842b3e1c600a105ced583fcaaf86aa3b2514b39
anzai <anzaiman.yamatogenius.2d@gmail.com>
2024-05-31 11:28:15 +0900

    feat:71,作成ボタンを押下した際に一覧画面に遷移するように修正

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -35,5 +36,11 @@ public class TaskController {
     @GetMapping("/tasks/creationForm")
     public String showCreationForm() {
         return "tasks/form";
+    }
+    
+    // POST /tasks
+    @PostMapping("/tasks")
+    public String create(Model model) {
+        return list(model);
     }
 }
