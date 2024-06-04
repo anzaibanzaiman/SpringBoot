23618d717a4a9ee67d85574111c040f59e5ddb8d
anzai <anzaiman.yamatogenius.2d@gmail.com>
2024-06-04 14:04:41 +0900

    refactor:92,EntityをFormに変換するコードのリファクタリング

--- src/main/java/com/example/todo/controller/task/TaskController.java
+++ src/main/java/com/example/todo/controller/task/TaskController.java
@@ -27,9 +27,10 @@ public class TaskController {
 
     @GetMapping("/{id}") // GET /tasks/detail
     public String showDetail(@PathVariable("id") long taskId, Model model){
-        var taskEntity = taskService.findById(taskId)
+        var taskDTO = taskService.findById(taskId)
+            .map(TaskDTO::toDTO)
                 .orElseThrow(TaskNotFoundException::new);
-        model.addAttribute("task", TaskDTO.toDTO(taskEntity));
+        model.addAttribute("task", taskDTO);
         return "tasks/detail";
     }
 
@@ -52,9 +53,9 @@ public class TaskController {
     // GET /tasks/{taskId}/editForm
     @GetMapping("/{id}/editForm")
     public String showEditForm(@PathVariable("id") long id, Model model) {
-        var taskEntity = taskService.findById(id)
+        var form = taskService.findById(id)
+                .map(TaskForm::formEntity)
                 .orElseThrow(TaskNotFoundException::new);
-        var form = new TaskForm(taskEntity.summary(), taskEntity.description(), taskEntity.status().name());
         model.addAttribute("taskForm", form);
         return "tasks/form";
     }
